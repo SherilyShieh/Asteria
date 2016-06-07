42e686ba76c5aa2b603db14539ecd720c1ddf3e1
SherilyShieh <1252524051@qq.com>
2016-06-07 18:22:36 +0800

    recyleview item样式

--- app/src/main/java/com/sherily/shieh/asteria/ui/RegisterMapActivity.java
+++ app/src/main/java/com/sherily/shieh/asteria/ui/RegisterMapActivity.java
@@ -8,13 +8,20 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Space;
 
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapPoi;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.TextureMapView;
+import com.baidu.mapapi.model.LatLng;
 import com.sherily.shieh.asteria.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit.http.PUT;
 
-public class RegisterMapActivity extends BaseActivity implements View.OnClickListener {
+public class RegisterMapActivity extends BaseActivity {
 
     @Bind(R.id.back)
     ImageView back;
@@ -29,18 +36,71 @@ public class RegisterMapActivity extends BaseActivity implements View.OnClickLis
     @Bind(R.id.recyclerView)
     RecyclerView recyclerView;
 
+    private BaiduMap mBaiduMap;
+
+    /**
+     * 覆盖物监听
+     */
+    private BaiduMap.OnMarkerClickListener mOnMarkerClickListener = new BaiduMap.OnMarkerClickListener() {
+        @Override
+        public boolean onMarkerClick(Marker marker) {
+            return true;
+        }
+    };
+    /**
+     *
+     */
+    private BaiduMap.OnMapClickListener mOnMapClickListener = new BaiduMap.OnMapClickListener() {
+        @Override
+        public void onMapClick(LatLng latLng) {
+
+        }
+
+        @Override
+        public boolean onMapPoiClick(MapPoi mapPoi) {
+            return false;
+        }
+    };
+
+    private BaiduMap.OnMapStatusChangeListener mOnMapStatusChangeListener = new BaiduMap.OnMapStatusChangeListener() {
+        @Override
+        public void onMapStatusChangeStart(MapStatus mapStatus) {
+
+        }
+
+        @Override
+        public void onMapStatusChange(MapStatus mapStatus) {
+
+        }
+
+        @Override
+        public void onMapStatusChangeFinish(MapStatus mapStatus) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_map);
         ButterKnife.bind(this);
-        back.setOnClickListener(this);
+        //地图相关初始化
+        mapView.showZoomControls(false);
+        mBaiduMap = mapView.getMap();
+        mBaiduMap.setOnMarkerClickListener(mOnMarkerClickListener);
+        mBaiduMap.setOnMapClickListener(mOnMapClickListener);
+        mBaiduMap.setOnMapStatusChangeListener(mOnMapStatusChangeListener);
 
+        //隐藏baiduMap logo
+        View logo = mapView.getChildAt(1);
+        logo.setVisibility(View.INVISIBLE);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
 
     }
 
@@ -57,9 +117,17 @@ public class RegisterMapActivity extends BaseActivity implements View.OnClickLis
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        // 关闭图层定位
+        mBaiduMap.setMyLocationEnabled(false);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+
     }
 
     private void onPre() {
@@ -67,13 +135,16 @@ public class RegisterMapActivity extends BaseActivity implements View.OnClickLis
         overridePendingTransition(R.anim.slide_left_in, R.anim.slide_right_out);
     }
 
+    @OnClick(R.id.back)
+    public void back() {
+        onPre();
+        this.finish();
+    }
+
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.back:
-                Intent intent= new Intent(RegisterMapActivity.this, RegisterActivity.class);
-                startActivity(intent);
-                break;
-        }
+    public void onBackPressed() {
+        super.onBackPressed();
+        onPre();
+        this.finish();
     }
 }
